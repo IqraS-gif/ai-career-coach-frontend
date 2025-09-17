4255dc84e10d6890d4275d9e82cb28bbe05d1902
Sayed Iqra Hassan <iqrahsayed625@gmail.com>
2025-09-17 19:12:31 +0530

    Update assessment.js

--- script/assessment.js
+++ script/assessment.js
@@ -1,6 +1,6 @@
 // frontend/script/assessment.js
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = 'https://ai-career-coach-backend-3.onrender.com';
 
 let currentUser = null;
 let assessmentQuestions = [];
